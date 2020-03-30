a4430862007f890f0b590c05e54ad21427fb4e17
Jack09jk <57107195+Jack09jk@users.noreply.github.com>
2020-03-30 17:03:40 +0530

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -7,7 +7,7 @@ module.exports = Object.freeze({
   ARIA_FILTERED_FILENAMES: [], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
   INDEX_DOMAIN: 'https://its.pronoobgdrivemoviess.workers.dev/Pronoobcloud/', //Get it from here https://gdindex-code-builder.glitch.me/
-  GDRIVE_PARENT_DIR_ID: '1skZrysCrlI6lWo2a2IIjHEDxxlmCtKpU',
+  GDRIVE_PARENT_DIR_ID: '13z5zCp5XR-XdyT50HdJDr84IzdroF-lE',
   SUDO_USERS: [882130858],	// Telegram user IDs. These users can use the bot in any chat.
   AUTHORIZED_CHATS: [-1001403349196],	// Telegram chat IDs. Anyone in these chats can use the bot.
   STATUS_UPDATE_INTERVAL_MS: 5000, // A smaller number will update faster, but might cause rate limiting
@@ -25,5 +25,5 @@ module.exports = Object.freeze({
     ENABLED: true,  // If true, all commands except '/list' has to have the bot username after the command
     NAME: "@CloudDriver_bot"
   },
-  IS_TEAM_DRIVE: false
+  IS_TEAM_DRIVE: true
 });
